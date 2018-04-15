4128b966259a562a01405e49ad5aa28a831e071c
techno-tanoC <develop.tanoc@gmail.com>
2018-04-15 22:12:51 +0900

    add app

---
+++ app/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Data.Yakushi.Type
+import Data.Yakushi.Parser
+-- import Data.Yakushi.Builder
+
+as :: Attributes
+as = [
+    ("a", ANull)
+   , ("b", ABool True)
+  ]
+
+ta :: Element
+ta = TA "tag" as 
+
+st :: Element
+st = Str "string element"
+
+tae :: Element
+tae = TAE "tag" as [ta, st]
+
+main :: IO ()
+main = return ()
